da3d570fcd1ca4a86106a419d80b096f98f86dd0
lukestroh <luke.strohbehn@gmail.com>
2024-12-08 13:02:40 -0800

    add admittance controller package

---
+++ src/admittance_controller/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import glob
+import os
+
+package_name = 'admittance_controller'
+
+setup(
+    name=package_name,
+    author=['lukestroh', 'Hannah Kolano'],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+         # Launch files
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/*launch.py')),
+        # Config files
+        (os.path.join('share', package_name, 'config'), glob.glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lukestroh',
+    maintainer_email='luke.strohbehn@gmail.com',
+    description='admittance controller package',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "wrench_filter_node = admittance_controller.wrench_filter:main",
+            "contact_watcher_node = admittance_controller.contact_watcher:main",
+            "fake_wrench_pub_node = admittance_controller.fake_wrench_publisher:main"
+        ],
+    },
+)
