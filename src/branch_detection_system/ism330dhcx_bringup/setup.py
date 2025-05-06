d27289d56c19e62636e6788c74d082d9e21f7988
lukestroh <luke.strohbehn@gmail.com>
2025-05-05 19:36:29 -0700

    added msgs and bringup packages for the ism330dhcx IMU

---
+++ src/branch_detection_system/ism330dhcx_bringup/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+import glob
+import os
+
+package_name = "ism330dhcx_bringup"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        # Launch files
+        (os.path.join("share", package_name, "launch"), glob.glob("launch/*launch.py")),
+        # Config files
+        # (os.path.join("share", package_name, "config"), glob.glob("config/*.yaml")),
+        # (os.path.join("share", package_name, "config"), glob.glob("config/*.json")),
+        # # Plotjuggler
+        (os.path.join("share", package_name, "plotjuggler"), glob.glob("plotjuggler/*.xml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="lukestroh",
+    maintainer_email="luke.strohbehn@gmail.com",
+    description="Bringup package for the ISM330DHCX IMU sensor",
+    license="BSD-3-Clause",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "ism330dhcx_filter_node = ism330dhcx_bringup.ism330dhcx_filter_node:main",
+        ],
+    },
+)
